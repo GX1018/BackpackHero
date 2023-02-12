2b151f47c61304f1b0c661cd29d955e0c8e1c5f5
GX1018 <wjdghlxo@gmail.com>
2023-02-13 07:44:34 +0900

    make test Scene

---
+++ BsckpackHero/Assets/Scripts/Item.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class Item : MonoBehaviour, IPointerDownHandler, 
+    IPointerUpHandler, IDragHandler
+{
+    public bool isClicked = false;
+    private RectTransform objRect = default;
+    // Start is called before the first frame update
+    void Start()
+    {
+        isClicked = false;
+        objRect = gameObject.GetComponent<RectTransform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        isClicked = true;
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        isClicked = false;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        if(isClicked == true)
+        {
+            gameObject.transform.position = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, 
+				Input.mousePosition.y, -Camera.main.transform.position.z));
+            
+            
+        }
+    }
+}
